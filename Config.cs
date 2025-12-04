f5e814f17c6921ab54e30e0bfe6fed9dee5cff08
Grevendev <edvin.lindborg@hotmail.se>
2025-12-04 16:02:55 +0100

    Added Program.cs Users.cs Config.cs Added Config and builder.Services on Program.cs Added Logic in Users.cs so we have something to work with. Added basic logic in Config.cs This is the begning of the REST-API

---
+++ Config.cs
@@ -0,0 +1,3 @@
+namespace TravelAgency;
+
+record Config(string ConnectionString);
--- Program.cs
+++ Program.cs
@@ -1,5 +1,18 @@
+using MySql.Data.MySqlClient;
+using TravelAgency;
+
 var builder = WebApplication.CreateBuilder(args);
+
+Config config = new("server=127.0.0.1;uid=root;pwd=rootroot;database=test");
+builder.Services.AddSingleton<Config>(config);
+builder.Services.AddDistributedMemoryCache();
+
+
+
 var app = builder.Build();
+
+app.MapPost("/register", Users.Post);
+
 
 app.MapGet("/", () => "Hello World!");
 
