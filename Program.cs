37aa4cb3a6c60a6cfb880d3c9f0e68bfa364e3c2
Grevendev <edvin.lindborg@hotmail.se>
2025-12-09 14:41:33 +0100

    Deleted comments

--- Program.cs
+++ Program.cs
@@ -89,7 +89,7 @@ static async Task<IResult> Users_Post_Handler(Users.Post_Args user, Config confi
     Users.RegistrationStatus.Success => Results.Created($"/register/{userId}", new { Message = "Account created." }),
     Users.RegistrationStatus.EmailConflict => Results.Conflict(new { Message = "Email already exists." }),
     Users.RegistrationStatus.InvalidFormat => Results.BadRequest(new { Message = "Unvalid format." }),
-    Users.RegistrationStatus.WeakPassword => Results.BadRequest(new { Message = "Weak-password. Minimum 15 characters." }),//Weak-password minimum 15char hela
+    Users.RegistrationStatus.WeakPassword => Results.BadRequest(new { Message = "Weak-password. Minimum 15 characters." }),
     _ => Results.StatusCode(500)
   };
 }
