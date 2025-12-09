00b3bab55a9e5bb09ed1824cba26afc515427cf3
jarl <jarllindquist@gmail.com>
2025-12-09 14:47:00 +0100

    adding hotels class
    starting to add tables and FK for the hotels and assosiates tables

--- Program.cs
+++ Program.cs
@@ -43,24 +43,33 @@ async Task db_reset_to_default(Config config)
   /* expire_date  DATE NOT NULL */
   );
 
-  DELIMITER $$
-  CREATE PROCEDURE create_password_request(IN p_email VARCHAR(255))
-  BEGIN
-    START TRANSACTION;
+  
+    CREATE TABLE Hotels
+  (
+  Id INT PRIMARY KEY AUTO_INCREMENT,
+  name varchar(100) NOT NULL, 
+  address VARCHAR(255) NOT NULL,
+  price_class INT NOT NULL,
+  rooms INT NOT NULL, 
+  floor INT NOT NULL,
+  breakfast BOOL NOT NULL DEFAULT FALSE
+  );     
 
-    INSERT INTO password_request (`user`)
-    SELECT u.id
-    FROM users u
-    WHERE u.email = p_email;
+  /*
+  CREATE TABLE Floors 
+  (
+  FloorId NOT NULL, 
 
-    IF ROW_COUNT() = 0 THEN
-      ROLLBACK;
-      SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'No user with that email';
-    END IF;
-
-    COMMIT;
-  END$$
-  DELIMITER ;
+  */
+  
+  CREATE TABLE Rooms
+  (
+   Id INT PRIMARY KEY AUTO_INCREMENT,
+   hotelsId INT NOT NULL,
+   how_many_beds INT NOT NULL, /* typeofroom*/
+   vacancy BOOL NOT NULL DEFAULT TRUE,
+   floor INT NOT NULL UNIQUE
+   );
 
   CREATE TABLE users
   (
@@ -69,11 +78,21 @@ async Task db_reset_to_default(Config config)
   first_name VARCHAR(50),
   last_name VARCHAR(100),
   date_of_birth DATE,
-  password VARCHAR(256))
+  password VARCHAR(256));
+
+
+  ALTER TABLE Hotels
+  ADD FOREIGN KEY (rooms) REFERENCES Rooms(Id);
+
+  ALTER TABLE Rooms
+  ADD FOREIGN KEY (hotelsId) REFERENCES Hotels(Id);
+
   """;
 
   await MySqlHelper.ExecuteNonQueryAsync(config.db, "DROP TABLE IF EXISTS users");
   await MySqlHelper.ExecuteNonQueryAsync(config.db, "DROP TABLE IF EXISTS password_request");
+  await MySqlHelper.ExecuteNonQueryAsync(config.db, "DROP TABLE IF EXISTS Rooms");
+  await MySqlHelper.ExecuteNonQueryAsync(config.db, "DROP TABLE IF EXISTS Hotels");
   await MySqlHelper.ExecuteNonQueryAsync(config.db, users_create);
   await MySqlHelper.ExecuteNonQueryAsync(config.db, "INSERT INTO users(email, first_name, last_name, date_of_birth, password) VALUES ('edvin@example.com', 'Edvin', 'Linconfig.dborg', '1997-08-20', 'travelagency')");
   // await MySqlHelper.ExecuteNonQueryAsync(config.db, "CALL create_password_request('edvin@example.com')");
@@ -93,6 +112,25 @@ static async Task<IResult> Users_Post_Handler(Users.Post_Args user, Config confi
     _ => Results.StatusCode(500)
   };
 }
+
+// DELIMITER $$
+//   CREATE PROCEDURE create_password_request(IN p_email VARCHAR(255))
+//   BEGIN
+//     START TRANSACTION;
+
+// INSERT INTO password_request (`user`)
+//     SELECT u.id
+//     FROM users u
+//     WHERE u.email = p_email;
+
+// IF ROW_COUNT() = 0 THEN
+//   ROLLBACK;
+// SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'No user with that email';
+// END IF;
+
+// COMMIT;
+// END$$
+//   DELIMITER ;
 
 
 //List<Users> UsersGet()
