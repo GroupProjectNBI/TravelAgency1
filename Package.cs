04d2420992fdc3a6a2850579a8721367269e4f3a
jarl <jarllindquist@gmail.com>
2025-12-12 13:38:00 +0100

    create enpoint for get one and get all packages
    handle request to database from packagase class file

--- Package.cs
+++ Package.cs
@@ -5,6 +5,53 @@ using MySql.Data.MySqlClient;
 
 class Package
 {
+    public record GetAll_Data(int id, int location_id, string name, string description, string package_type);
+    public static async Task<List<GetAll_Data>>
+    GetAll(Config config)
+    {
+        List<GetAll_Data> restult = new();
+        string get_all_query = "SELECT id,location_id,name, description, package_type FROM packages";
+        using (var reader = await
+        MySqlHelper.ExecuteReaderAsync(config.db, get_all_query))
+        {
+            while (reader.Read())
+            {
+                restult.Add(new(
+                    reader.GetInt32(0),
+                    reader.GetInt32(1),
+                    reader.GetString(2),
+                    reader.GetString(3),
+                    reader.GetString(4)
+                ));
+            }
+        }
+        return restult;
+    }
+
+    public record Get_Data(int location_id, string name, string description, string package_type);
+    public static async Task<Get_Data>
+    Get(int Id, Config config)
+    {
+        Get_Data? result = null;
+        string get_one_query = "SELECT location_id,name, description, package_type FROM packages WHERE id = @id ";
+        var parameters = new MySqlParameter[] { new("@Id", Id) };
+
+        using (var reader = await
+        MySqlHelper.ExecuteReaderAsync(config.db, get_one_query, parameters))
+        {
+            if (reader.Read())
+            {
+                result = new(
+                  reader.GetInt32(0),
+                  reader.GetString(1),
+                  reader.GetString(2),
+                  reader.GetString(3)
+                );
+            }
+        }
+        return result;
+    }
+
     public record Post_Args(int location_id, string name, string description, int package_type);
     public static async Task<IResult>
     Post(Post_Args args, Config config)
