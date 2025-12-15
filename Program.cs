446c134244e0efb48479181c594d9d74718c554f
ceccav <ceciliacavallin@hotmail.se>
2025-12-15 13:50:51 +0100

    corrected endpoint

--- Program.cs
+++ Program.cs
@@ -108,7 +108,7 @@ app.MapPost("/bookings", Bookings.Post);
 
 //endpoit for booking meals
 app.MapPut("/booking_meals/{id}", bookings_meals.Put);
-app.MapPut("/booking_meals/{id}", bookings_meals.Delete);
+app.MapDelete("/booking_meals/{id}", bookings_meals.Delete);
 
 
 app.Run();
