d0fe43d85a62492901738194bd6b2598716a17c0
KHS1993 <kifle.selassie@student.nbi-handlesakademin.se>
2025-12-15 13:50:19 +0100

    Added endpoint
    added endpoint for bokkings delete

--- Program.cs
+++ Program.cs
@@ -103,6 +103,7 @@ handler funktionerna undertill är bara mer kod som måste hanteras.
 */
 app.MapGet("/bookings", Bookings_Get_All_Handler);
 app.MapPost("/bookings", Bookings.Post);
+app.MapPost("/bookings/{id}", Bookings.Delete);
 
 
 
